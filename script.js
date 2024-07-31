37e7d57042d535bd1595bc8849aff60d7f1363fa
Nikolay Miller <miller.ni.vl@gmail.com>
2024-07-31 07:02:36 +0300

    added joke-api and connected with text-to-speech

--- script.js
+++ script.js
@@ -6,10 +6,16 @@ const button = document.getElementById('button')
 const VoiceRSS = { speech: function (e) { this._validate(e), this._request(e) }, _validate: function (e) { if (!e) throw "The settings are undefined"; if (!e.key) throw "The API key is undefined"; if (!e.src) throw "The text is undefined"; if (!e.hl) throw "The language is undefined"; if (e.c && "auto" != e.c.toLowerCase()) { var a = !1; switch (e.c.toLowerCase()) { case "mp3": a = (new Audio).canPlayType("audio/mpeg").replace("no", ""); break; case "wav": a = (new Audio).canPlayType("audio/wav").replace("no", ""); break; case "aac": a = (new Audio).canPlayType("audio/aac").replace("no", ""); break; case "ogg": a = (new Audio).canPlayType("audio/ogg").replace("no", ""); break; case "caf": a = (new Audio).canPlayType("audio/x-caf").replace("no", "") }if (!a) throw "The browser does not support the audio codec " + e.c } }, _request: function (e) { var a = this._buildRequest(e), t = this._getXHR(); t.onreadystatechange = function () { if (4 == t.readyState && 200 == t.status) { if (0 == t.responseText.indexOf("ERROR")) throw t.responseText; audioElement.src = t.responseText, audioElement.play() } }, t.open("POST", "https://api.voicerss.org/", !0), t.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8"), t.send(a) }, _buildRequest: function (e) { var a = e.c && "auto" != e.c.toLowerCase() ? e.c : this._detectCodec(); return "key=" + (e.key || "") + "&src=" + (e.src || "") + "&hl=" + (e.hl || "") + "&r=" + (e.r || "") + "&c=" + (a || "") + "&f=" + (e.f || "") + "&ssml=" + (e.ssml || "") + "&b64=true" }, _detectCodec: function () { var e = new Audio; return e.canPlayType("audio/mpeg").replace("no", "") ? "mp3" : e.canPlayType("audio/wav").replace("no", "") ? "wav" : e.canPlayType("audio/aac").replace("no", "") ? "aac" : e.canPlayType("audio/ogg").replace("no", "") ? "ogg" : e.canPlayType("audio/x-caf").replace("no", "") ? "caf" : "" }, _getXHR: function () { try { return new XMLHttpRequest } catch (e) { } try { return new ActiveXObject("Msxml3.XMLHTTP") } catch (e) { } try { return new ActiveXObject("Msxml2.XMLHTTP.6.0") } catch (e) { } try { return new ActiveXObject("Msxml2.XMLHTTP.3.0") } catch (e) { } try { return new ActiveXObject("Msxml2.XMLHTTP") } catch (e) { } try { return new ActiveXObject("Microsoft.XMLHTTP") } catch (e) { } throw "The browser does not support HTTP request" } };
 
 
-function test() {
+// Disable/Enable Button
+const toggleButton = () => {
+	button.disabled = !button.disabled
+}
+
+// Passing Joke to VoiceRSS API
+const tellMe = async (joke) => {
 	VoiceRSS.speech({
 		key: 'bea5ca153b0849f8b9bf415d2b1c00c9',
-		src: 'Hello, world',
+		src: joke,
 		hl: 'en-us',
 		r: 0,
 		c: 'mp3',
@@ -18,4 +24,22 @@ function test() {
 	})
 }
 
-test()
+// Get Jokes from Joke API
+const getJokes = async () => {
+	toggleButton()
+	let joke = ''
+	const url = 'https://v2.jokeapi.dev/joke/Any';
+	try {
+		const res = await fetch(url)
+		const data = await res.json()
+		joke = data.setup ? `${data.setup} ... ${data.delivery}` : data.joke
+		tellMe(joke)
+	} catch (error) {
+		console.log('Whoops', error)
+	}
+}
+
+
+
+button.addEventListener('click', getJokes)
+audioElement.addEventListener('ended', toggleButton)
